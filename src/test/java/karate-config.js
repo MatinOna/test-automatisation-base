1ebbf66e114a4b1e430275b528d9c8ca8fa523c4
mpona <mpona@pichincha.com>
2025-06-20 10:53:02 -0500

    ADD get and delete test

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -7,15 +7,17 @@ function() {
   var config = {
     env: env,
 	baseUrl: 'https://petstore.swagger.io/v2/user',
-    urlGetCharacters: 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters'
+    urlGetCharacters: 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters',
+    urlGetCharacterById: 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters/1',
   }
   if (env == 'test') {
 
     config.urlGetCharacters = 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters';
+    config.urlGetCharacterById = 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters/1';
     // e.g. config.foo = 'bar';
   } else if (env == 'prod') {
-
     config.urlGetCharacters = 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters';
+    config.urlGetCharacterById = 'http://bp-se-test-cabcd9b246a5.herokuapp.com/mpona/api/characters/1';
   }
   karate.configure('connectTimeout', 15000);
   karate.configure('readTimeout', 15000);
--- src/test/resources/karate-test.feature
+++ src/test/resources/karate-test.feature
@@ -1,9 +1,20 @@
-Feature: Test de API súper simple
+@SEGCD-001
+Feature: Character CRUD
 
   Background:
     * configure ssl = true
 
-  Scenario: Verificar que un endpoint público responde 200
+  @id:1
+  Scenario: Check the acquisition of the characters
     Given url urlGetCharacters
     When method get
     Then status 200
+
+  @id:2
+  Scenario: Check the acquisition of the characters for ID
+    Given url urlGetCharacterById
+    When method get
+    Then status 200
+    And match response.name == "Iron Man"
+    And match response.alterego == "Tony Stark"
+
